a7eaddd61e4063c3ea186c6368b685d9c16a893a
suhyun choi <shchoi@soft-berry.com>
2023-03-19 23:30:40 +0900

    feat : 충전기정보 추가

---
+++ app/src/main/java/com/example/testevinfra/data/ChargerType.kt
@@ -0,0 +1,25 @@
+package com.example.testevinfra.data
+
+const val CHARGER_TYPE_DCDEMO = 1 // DC차데모
+
+const val CHARGER_TYPE_SLOW = 2 // 완속
+
+const val CHARGER_TYPE_DCDEMO_AC = 3 // DC차데모 + AC3상
+
+const val CHARGER_TYPE_DCCOMBO = 4 // DC콤보
+
+const val CHARGER_TYPE_DCDEMO_DCCOMBO = 5 // DC차데모 + DC콤보
+
+const val CHARGER_TYPE_DCDEMO_DCCOMBO_AC = 6 // DC차데모 + DC콤보 + AC3상
+
+const val CHARGER_TYPE_AC = 7 // AC3상
+
+const val CHARGER_TYPE_DCCOMBO_AC = 8 // DC콤보+AC3상
+
+const val CHARGER_TYPE_ETC = 9 // 기타
+
+const val CHARGER_TYPE_HYDROGEN = 10 // 수소연료자동차
+
+const val CHARGER_TYPE_SUPER_CHARGER = 11 // 수퍼차저
+
+const val CHARGER_TYPE_DESTINATION = 12 // 데스티네이션
--- app/src/main/java/com/example/testevinfra/data/StationInfo.kt
+++ app/src/main/java/com/example/testevinfra/data/StationInfo.kt
@@ -1,4 +1,4 @@
-package com.example.testevinfra
+package com.example.testevinfra.data
 
 data class StationInfo (
     val id: String,
@@ -6,11 +6,53 @@ data class StationInfo (
     val op: String,
     val lat: String,
     val lon: String,
-    val cl: List<Charger>
-    )
+    val cl: List<Charger>,
+    val roof: String,
+    val st: Int
+    ){
+    val totalType: String get() {
+        return when (st) {
+            CHARGER_TYPE_DCDEMO -> "DC 차데모"
+            CHARGER_TYPE_DCDEMO_AC -> "DC 차데모 • AC 3상"
+            CHARGER_TYPE_DCCOMBO -> "DC 콤보"
+            CHARGER_TYPE_DCDEMO_DCCOMBO -> "DC 차데모 • DC 콤보"
+            CHARGER_TYPE_DCDEMO_DCCOMBO_AC -> "DC 차데모 • DC 콤보 • AC 3상"
+            CHARGER_TYPE_AC -> "AC 3상"
+            CHARGER_TYPE_SUPER_CHARGER -> "수퍼차저"
+            CHARGER_TYPE_DESTINATION -> "데스티네이션"
+            else -> ""
+        }
+    }
+    val lmStr: String get() {
+        val hasLimit = cl.any {
+            it.lm == "N" || it.lm == "UN"
+        }
+        return if (hasLimit) "비개방" else "개방"
+    }
+
+    val roofStr: String get() {
+        return when(roof) {
+            "0" -> "야외"
+            "1" -> "실내"
+            "2" -> "캐노피"
+            else -> ""
+        }
+    }
+
+    val standard:List<Charger> get() = cl.filter { it.p.toInt() < 50 }
+    val standardActive:List<Charger> get() = cl.filter { it.p.toInt() < 50 && it.cst == "2"}
+    val fast:List<Charger> get() =  cl.filter { it.p.toInt() >= 50 }
+    val fastActive:List<Charger> get() =  cl.filter { it.p.toInt() >= 50 && it.cst == "2" }
+}
 
 data class Charger (
     val cid: String,
     val cst: String,
-    val p: String
-    )
+    val p: String,
+    val tid: String,
+    val rd: String?,
+    val method: String,
+    val s: String?,
+    val lm: String
+    ){
+}
